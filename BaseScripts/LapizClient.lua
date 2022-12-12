6ea50986836ec352f99dc7cee6518b111f287d27
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-12-12 12:24:50 -0800

    created client file

---
+++ BaseScripts/LapizClient.lua
@@ -0,0 +1,18 @@
+--[[
+
+Created by OceanTubez.
+
+Version 1.0
+
+Lapiz Gun System (client)
+
+--]]
+
+local tool = script.Parent
+
+local Remotes = {
+  ChangeMagAmmo = tool:WaitForChild("ChangeMagAmmo"),
+  Shoot = tool:WaitForChild("Shoot")
+}
+
+local UIS = game:GetService("UserInputService")
