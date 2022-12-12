73deb57f8257723a4fb97071a0dad7538947c9ce
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-12-12 14:15:28 -0800

    updated server script

--- BaseScripts/LapizServer.lua
+++ BaseScripts/LapizServer.lua
@@ -7,7 +7,48 @@ Version 1.0
 Lapiz Gun System (server)
 
 --]]
-
 local tool = script.Parent
+local assets = tool:WaitForChild("Assets")
 
-local config = require(tool:WaitForChild("Configuration"))
+local handle = tool:WaitForChild("Handle")
+
+local settingmodule = require(tool:WaitForChild("Configuration"))
+local FastCast = require(assets:WaitForChild("FastCastRedux"))
+
+local config = settingmodule.Settings
+
+local Remotes = {
+  ChangeMagAmmo = tool:WaitForChild("ChangeMagAmmo"),
+  Shoot = tool:WaitForChild("Shoot"),
+  Inspect = tool:WaitForChild("Inspect")
+}
+
+-- Setting up ammo
+
+local curAmmo = handle:WaitForChild("Ammo")
+local maxAmmo = handle:WaitForChild("MaxAmmo")
+local spareAmmo = handle:WaitForChild("SpareAmmo")
+local maxspareAmmo = handle:WaitForChild("MaxSpareAmmo")
+
+curAmmo.Value = config.AmmoPerMag
+maxAmmo.Value = config.MaxAmmoPerMag
+spareAmmo.Value = config.StartingAmmo
+maxspareAmmo.Value = config.MaxAmmo
+
+-- Setting up animations
+
+local Anims = {
+  IdleAnim = tool:WaitForChild("Idle"),
+  ReloadAnim = tool:WaitForChild("Reload"),
+  InspectAnim = tool:WaitForChild("Inspect"),
+  ShootAnim = tool:WaitForChild("Shoot")
+}
+
+Anims.IdleAnim.AnimationId = config.IdleAnimation
+
+Remotes.ChangeMagAmmo.OnServerEvent:Connect(function()
+    
+    
+    
+end)
+
