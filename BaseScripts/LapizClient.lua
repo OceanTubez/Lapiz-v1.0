3574988703c7410ababf47d7b7c268d7a992d807
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-12-12 12:46:06 -0800

    completed client script

--- BaseScripts/LapizClient.lua
+++ BaseScripts/LapizClient.lua
@@ -12,7 +12,32 @@ local tool = script.Parent
 
 local Remotes = {
   ChangeMagAmmo = tool:WaitForChild("ChangeMagAmmo"),
-  Shoot = tool:WaitForChild("Shoot")
+  Shoot = tool:WaitForChild("Shoot"),
+  Inspect = tool:WaitForChild("Inspect")
 }
 
 local UIS = game:GetService("UserInputService")
+
+UIS.InputBegan:Connect(function(input, gpe)
+    
+    if input.InputType == Enum.InputType.Keyboard then
+      
+      if input.KeyCode == Enum.KeyCode.R then
+        
+        Remotes.ChangeMagAmmo:FireServer()
+        
+      elseif input.KeyCode == Enum.KeyCode.F then
+        
+        Remotes.Inspect:FireServer()
+        
+      end
+      
+    end
+    
+end)
+
+tool.Activated:Connect(function()
+    
+    Remotes.Shoot:FireServer()
+    
+end)
