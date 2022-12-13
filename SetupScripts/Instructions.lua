fcb8719808c1fc9fa0997113038fb52e9dac2843
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-12-13 08:42:54 -0800

    created simple instructions

--- SetupScripts/Instructions.lua
+++ SetupScripts/Instructions.lua
@@ -1,6 +1,19 @@
 --[[
 
 
+Lapiz 1.0 Instruction Manual
+
+Created by OceanTubez
+
+Hello!
+This is the Instruction Manual for Lapiz v1.0!
+
+How to create your gun model:
+You need a part called 'BodyAttach' in your GunModel. You also need to rig the gun up properly
+for animations. Oh forgot, you also need another part called 'FirePoint' inside the GunModel.
+
+If you don't have these important parts in your gun, it may throw some warnings at you.
+You can look at the documentation on how to fix these and what they are.
 
 
 
