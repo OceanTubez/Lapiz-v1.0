444e5abd053a678ef574732376ce08342e10a0d5
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-12-12 22:22:22 -0800

    updated last night

--- BaseScripts/LapizClient.lua
+++ BaseScripts/LapizClient.lua
@@ -9,35 +9,36 @@ Lapiz Gun System (client)
 --]]
 
 local tool = script.Parent
+local remotefolder = tool:WaitForChild("Events")
 
 local Remotes = {
-  ChangeMagAmmo = tool:WaitForChild("ChangeMagAmmo"),
-  Shoot = tool:WaitForChild("Shoot"),
-  Inspect = tool:WaitForChild("Inspect")
+	ChangeMagAmmo = remotefolder:WaitForChild("ChangeMagAmmo"),
+	Shoot = remotefolder:WaitForChild("Shoot"),
+	Inspect = remotefolder:WaitForChild("Inspect")
 }
 
 local UIS = game:GetService("UserInputService")
 
 UIS.InputBegan:Connect(function(input, gpe)
-    
-    if input.InputType == Enum.InputType.Keyboard then
-      
-      if input.KeyCode == Enum.KeyCode.R then
-        
-        Remotes.ChangeMagAmmo:FireServer()
-        
-      elseif input.KeyCode == Enum.KeyCode.F then
-        
-        Remotes.Inspect:FireServer()
-        
-      end
-      
-    end
-    
+
+	if input.InputType == Enum.InputType.Keyboard then
+
+		if input.KeyCode == Enum.KeyCode.R then
+
+			Remotes.ChangeMagAmmo:FireServer()
+
+		elseif input.KeyCode == Enum.KeyCode.F then
+
+			Remotes.Inspect:FireServer()
+
+		end
+
+	end
+
 end)
 
 tool.Activated:Connect(function()
-    
-    Remotes.Shoot:FireServer()
-    
+
+	Remotes.Shoot:FireServer()
+
 end)
