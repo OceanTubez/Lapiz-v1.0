b51f676e27fe5a3bc47ee0e7b149ff4eec562496
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-12-13 17:01:33 -0800

    latest

--- BaseGunScripts/LapizClient.lua
+++ BaseGunScripts/LapizClient.lua
@@ -7,14 +7,20 @@ Version 1.0
 Lapiz Gun System (client)
 
 --]]
-
 local tool = script.Parent
+local config = require(tool:WaitForChild("Configuration")).Settings
 local remotefolder = tool:WaitForChild("Events")
+local bindablesfolder = game:GetService("ReplicatedStorage"):WaitForChild("Lapiz"):WaitForChild("ClientBindables")
 
 local Remotes = {
 	ChangeMagAmmo = remotefolder:WaitForChild("ChangeMagAmmo"),
 	Shoot = remotefolder:WaitForChild("Shoot"),
 	Inspect = remotefolder:WaitForChild("Inspect")
+}
+
+local Bindables = {
+	SetupGui = bindablesfolder:WaitForChild("SetupGui"),
+	Update = bindablesfolder:WaitForChild("Update")
 }
 
 local UIS = game:GetService("UserInputService")
@@ -41,4 +47,19 @@ tool.Activated:Connect(function()
 
 	Remotes.Shoot:FireServer()
 
+end)
+
+tool.Equipped:Connect(function()
+	
+	local char = tool.Parent
+	game.ReplicatedStorage.Lapiz.ServerEvents.ConnectM6D:FireServer(config.GunModel.BodyAttach)
+	char.Torso.ToolGrip.Part0 = char.Torso
+	char.Torso.ToolGrip.Part1 = config.GunModel.BodyAttach
+	
+end)
+
+tool.Unequipped:Connect(function()
+	
+	game.ReplicatedStorage.Lapiz.ServerEvents.DisconnectM6D:FireServer()
+	
 end)
