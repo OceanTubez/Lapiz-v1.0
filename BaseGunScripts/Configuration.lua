61f98847bf3174ad8c395dc051fc513e22f78840
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-12-14 22:03:01 -0800

    made latest change

--- BaseGunScripts/Configuration.lua
+++ BaseGunScripts/Configuration.lua
@@ -1,17 +1,17 @@
 local config = {}
 
 config.Settings = {
-	
+
 	-- REALLY IMPORTANT
-	
+
 	GunModel = script.Parent:WaitForChild("Gun"), -- Reference your gun model for the weapon
 
 	Firemode = "Semi", -- Can be any of these: Auto, Semi
 	Firerate = 0.1, -- In seconds (how many secs per bullet for auto)
-	BaseDamage = 20, -- Damage for all body parts excluding head
+	BaseDamage = 25, -- Damage for all body parts excluding head
 	AmmoPerMag = 12, -- Ammo per magazine before having to reload (also includes starting ammo)
 	MaxAmmoPerMag = 12, -- max ammo per magazine
-	BulletSpeed = 100, -- bullet velocity (higher = faster)
+	BulletSpeed = 1000, -- bullet velocity (higher = faster)
 
 	-- Reload configuration
 
@@ -30,30 +30,25 @@ config.Settings = {
 	HeadshotMultiEnabled = false, -- multiply base damage instead of setting damage for headshots?
 	HeadMultiplier = 1, -- How much damage is multiplied by if multiplier is enabled
 	HeadshotDamageEnabled = true, -- set damage instead of multiplying base damage for headshots?
-	HeadshotDamage = 45, -- How much damage per headshot if damage is enabled
+	HeadshotDamage = 10, -- How much damage per headshot if damage is enabled
 
 	-- Animation configuration
 
-	IdleAnimation = "rbxassetid://0", -- Set to "rbxassetid://0" if none (must be looped if it is existing) priority idle
-	FireAnimation = "rbxassetid://0", -- must be priority action
-	ReloadAnimation = "rbxassetid://0", -- must be proority action
-	InspectAnimation = "rbxassetid://0", -- must also be action
+	IdleAnimation = "rbxassetid://9065227216", -- Set to nil if none (must be looped if it is existing) priority idle
+	FireAnimation = "rbxassetid://9065222174", -- must be priority action
+	ReloadAnimation = "rbxassetid://9065224798", -- must be proority action
+	InspectAnimation = "rbxassetid://9065227216",
 
 	-- Viewmodel configuration [BETA] (feature enabled no choice yet)
 
 	ViewModelEnabled = true, -- recommended to be turned on
 	WalkCycle = true, -- Bobbing when moving?
 	Sway = true, -- Swaying when moving camera?
-
-	-- Sounds
-
-	-- For normal sounds, check the sounds folder.
-
-	-- For the 'precise' sounds, please go to each remote event connection to do that.
-
 	
-
-	}
+	-- FastCast config [BETA] (for more config go to fastcast config scritp itself)
+	
+	FastCastCustomizationEnabled = true,
+	BulletTemplate = game:GetService("ReplicatedStorage"):WaitForChild("Lapiz"):WaitForChild("BulletTemplates"):WaitForChild("Bullet") -- Custom Bullet (reference here)
 
 }
 
