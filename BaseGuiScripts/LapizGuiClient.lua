9f93a0d99471a20e7ae682474ddc31a9c65dd657
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-12-14 22:04:17 -0800

    latest version of da thing

--- BaseGuiScripts/LapizGuiClient.lua
+++ BaseGuiScripts/LapizGuiClient.lua
@@ -1,30 +1,30 @@
 local RS = game:GetService("ReplicatedStorage")
-local Bindables = RS:WaitForChild("LapizObjects"):WaitForChild("ClientBindables")
+local Bindables = RS:WaitForChild("Lapiz"):WaitForChild("ClientBindables")
 
 local Bindables = {
-    SetupGui = Bindables:WaitForChild("SetupGui"),
-    UpdateClient = Bindables:WaitForChild("UpdateClient")
+	SetupGui = Bindables:WaitForChild("SetupGui"),
+	UpdateClient = Bindables:WaitForChild("UpdateClient")
 }
 
 local Gui = script.Parent
 local MainFrame = Gui:WaitForChild("MainFrame")
 
 local DisplayLabels = {
-    GunName = MainFrame:WaitForChild("GunName"),
-    CurrentAmmo = MainFrame:WaitForChild("CurrentAmmo"),
-    SpareAmmo = MainFrame:WaitForChild("SpareAmmo")
+	GunName = MainFrame:WaitForChild("GunName"),
+	Ammo = MainFrame:WaitForChild("AmmoDisplay")
 }
 
 Bindables.SetupGui.Event:Connect(function(visible)
 
-    MainFrame.Visible = visible
+	MainFrame.Visible = visible
 
 end)
 
 Bindables.UpdateClient.Event:Connect(function(currentammo, spareammo, toolname)
-
-    DisplayLabels.GunName = toolname
-    DisplayLabels.CurrentAmmo = currentammo
-    DisplayLabels.SpareAmmo = spareammo
+	
+	print("recieve on gui")
+	
+	DisplayLabels.GunName.Text = toolname
+	DisplayLabels.Ammo.Text = currentammo.." / "..spareammo
 
 end)
